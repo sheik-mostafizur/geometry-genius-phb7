9263ccdfc4c0de60069aa0ae1946c84546a58352
sheik-mostafizur <sheikmostafizur2001@gmail.com>
2023-02-17 23:14:57 +0600

    getValueByInput and return floating number

---
+++ assets/js/utility.js
@@ -0,0 +1,8 @@
+// get value form input and converted floating number
+function getValueByIdInput(elementId) {
+  const element = document.getElementById(elementId);
+  const elementValue = parseFloat(element.value);
+  return elementValue;
+}
+
+
